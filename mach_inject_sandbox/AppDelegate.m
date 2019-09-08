5fb292d175056aab9ee6181b58120b4d57941c95
Topiga <topiga.frt@gmail.com>
2019-09-08 21:52:29 +0200

    Some fixes, not working tho

--- mach_inject_sandbox/AppDelegate.m
+++ mach_inject_sandbox/AppDelegate.m
@@ -5,9 +5,8 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
 	printf("lol\n");
-	mach_error_t err = mach_inject_bundle_pid( [[[NSBundle mainBundle] pathForResource:@"mach_inject_sandbox_bundle"
-																				ofType:@"bundle"] fileSystemRepresentation],
-											   getpid() );
+	mach_error_t err = mach_inject_bundle_pid( [[[NSBundle mainBundle] pathForResource:@"mach_inject_sandbox_bundle" ofType:@"bundle"] fileSystemRepresentation],
+		getpid() );
 	assert( !err );
 }
 
